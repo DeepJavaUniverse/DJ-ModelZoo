aca4284e36dfd1f10ced9941223dff83c578a228
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2018-02-24 14:35:37 -0800

    MNIst downloader now supports unziping the files

--- src/main/java/com/kovalevskyi/java/deep/models/mnist/Downloader.java
+++ src/main/java/com/kovalevskyi/java/deep/models/mnist/Downloader.java
@@ -3,10 +3,14 @@ package com.kovalevskyi.java.deep.models.mnist;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.util.zip.GZIPInputStream;
 
 public abstract class Downloader {
 
@@ -18,14 +22,23 @@ public abstract class Downloader {
     private static final String MNIST_TEST_SET_LABELS = "/exdb/mnist/t10k-labels-idx1-ubyte.gz";
     private static final String TMP_DIR_PATH = System.getProperty("java.io.tmpdir");
     
-    public static final File MNIST_TRAIN_SET_IMAGES_FILE_PATH
+    private static final File MNIST_TRAIN_SET_IMAGES_ZIP_FILE
             = Paths.get(TMP_DIR_PATH, "train-images.gz").toFile();
-    public static final File MNIST_TRAIN_SET_LABELS_FILE_PATH
+    private static final File MNIST_TRAIN_SET_LABELS_ZIP_FILE
             = Paths.get(TMP_DIR_PATH, "train-labels.gz").toFile();
-    public static final File MNIST_TEST_SET_IMAGES_FILE_PATH
+    private static final File MNIST_TEST_SET_IMAGES_ZIP_FILE
             = Paths.get(TMP_DIR_PATH, "test-images.gz").toFile();
-    public static final File MNIST_TEST_SET_LABELS_FILE_PATH
+    private static final File MNIST_TEST_SET_LABELS_ZIP_FILE
             = Paths.get(TMP_DIR_PATH, "test-images.gz").toFile();
+    
+    public static final File MNIST_TRAIN_SET_IMAGES_FILE
+            = Paths.get(TMP_DIR_PATH, "train-images").toFile();
+    public static final File MNIST_TRAIN_SET_LABELS_FILE
+            = Paths.get(TMP_DIR_PATH, "train-labels").toFile();
+    public static final File MNIST_TEST_SET_IMAGES_FILE
+            = Paths.get(TMP_DIR_PATH, "test-images").toFile();
+    public static final File MNIST_TEST_SET_LABELS_FILE
+            = Paths.get(TMP_DIR_PATH, "test-images").toFile();
 
     private Downloader() { }
 
@@ -44,28 +57,50 @@ public abstract class Downloader {
             throw new RuntimeException("Failure to create URLs that are required to download MNIst DataSet", e);
         }
         try {
-            FileUtils.copyURLToFile(trainSetImages, MNIST_TRAIN_SET_IMAGES_FILE_PATH);
-            FileUtils.copyURLToFile(trainSetLabels, MNIST_TRAIN_SET_LABELS_FILE_PATH);
-            FileUtils.copyURLToFile(testSetImages, MNIST_TEST_SET_IMAGES_FILE_PATH);
-            FileUtils.copyURLToFile(testSetLabels, MNIST_TEST_SET_LABELS_FILE_PATH);
+            FileUtils.copyURLToFile(trainSetImages, MNIST_TRAIN_SET_IMAGES_ZIP_FILE);
+            FileUtils.copyURLToFile(trainSetLabels, MNIST_TRAIN_SET_LABELS_ZIP_FILE);
+            FileUtils.copyURLToFile(testSetImages, MNIST_TEST_SET_IMAGES_ZIP_FILE);
+            FileUtils.copyURLToFile(testSetLabels, MNIST_TEST_SET_LABELS_ZIP_FILE);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException("Failure to download MNIst DataSet", e);
         }
+
+        unZipFile(MNIST_TRAIN_SET_IMAGES_ZIP_FILE, MNIST_TRAIN_SET_IMAGES_FILE);
+        unZipFile(MNIST_TRAIN_SET_LABELS_ZIP_FILE, MNIST_TRAIN_SET_LABELS_FILE);
+        unZipFile(MNIST_TEST_SET_IMAGES_ZIP_FILE, MNIST_TEST_SET_IMAGES_FILE);
+        unZipFile(MNIST_TEST_SET_LABELS_ZIP_FILE, MNIST_TEST_SET_LABELS_FILE);
     }
 
     public static void clearDownloadedFiles() {
-        if (MNIST_TRAIN_SET_IMAGES_FILE_PATH.exists()) {
-            MNIST_TRAIN_SET_IMAGES_FILE_PATH.delete();
-        }
-        if (MNIST_TRAIN_SET_LABELS_FILE_PATH.exists()) {
-            MNIST_TRAIN_SET_LABELS_FILE_PATH.delete();
-        }
-        if (MNIST_TEST_SET_IMAGES_FILE_PATH.exists()) {
-            MNIST_TEST_SET_IMAGES_FILE_PATH.delete();
-        }
-        if (MNIST_TEST_SET_LABELS_FILE_PATH.exists()) {
-            MNIST_TEST_SET_LABELS_FILE_PATH.delete();
+        removeFilesIfExist(
+                MNIST_TRAIN_SET_IMAGES_ZIP_FILE,
+                MNIST_TRAIN_SET_LABELS_ZIP_FILE,
+                MNIST_TEST_SET_IMAGES_ZIP_FILE,
+                MNIST_TEST_SET_LABELS_ZIP_FILE,
+                MNIST_TRAIN_SET_IMAGES_FILE,
+                MNIST_TRAIN_SET_LABELS_FILE,
+                MNIST_TEST_SET_IMAGES_FILE,
+                MNIST_TEST_SET_LABELS_FILE);
+    }
+
+    private static void removeFilesIfExist(final File... files) {
+       Stream.of(files).filter(File::exists).forEach(File::delete);
+    }
+
+    private static void unZipFile(final File fileToUnzip, final File dest) {
+        final byte[] buffer = new byte[1024];
+
+        try(GZIPInputStream src = new GZIPInputStream(new FileInputStream(fileToUnzip));
+            FileOutputStream dst = new FileOutputStream(dest)) {
+
+            int len;
+            while ((len = src.read(buffer)) > 0) {
+                dst.write(buffer, 0, len);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Unzip process have failed", e);
         }
     }
 }
--- src/test/java/com/kovalevskyi/java/deep/models/mnist/DownloaderTest.java
+++ src/test/java/com/kovalevskyi/java/deep/models/mnist/DownloaderTest.java
@@ -9,19 +9,19 @@ public class DownloaderTest {
     @Test
     public void name() {
         Downloader.clearDownloadedFiles();
-        assertFalse(Downloader.MNIST_TRAIN_SET_IMAGES_FILE_PATH.exists());
-        assertFalse(Downloader.MNIST_TRAIN_SET_LABELS_FILE_PATH.exists());
-        assertFalse(Downloader.MNIST_TEST_SET_IMAGES_FILE_PATH.exists());
-        assertFalse(Downloader.MNIST_TEST_SET_LABELS_FILE_PATH.exists());
+        assertFalse(Downloader.MNIST_TRAIN_SET_IMAGES_FILE.exists());
+        assertFalse(Downloader.MNIST_TRAIN_SET_LABELS_FILE.exists());
+        assertFalse(Downloader.MNIST_TEST_SET_IMAGES_FILE.exists());
+        assertFalse(Downloader.MNIST_TEST_SET_LABELS_FILE.exists());
         Downloader.downloadMnist();
-        assertTrue(Downloader.MNIST_TRAIN_SET_IMAGES_FILE_PATH.exists());
-        assertTrue(Downloader.MNIST_TRAIN_SET_LABELS_FILE_PATH.exists());
-        assertTrue(Downloader.MNIST_TEST_SET_IMAGES_FILE_PATH.exists());
-        assertTrue(Downloader.MNIST_TEST_SET_LABELS_FILE_PATH.exists());
+        assertTrue(Downloader.MNIST_TRAIN_SET_IMAGES_FILE.exists());
+        assertTrue(Downloader.MNIST_TRAIN_SET_LABELS_FILE.exists());
+        assertTrue(Downloader.MNIST_TEST_SET_IMAGES_FILE.exists());
+        assertTrue(Downloader.MNIST_TEST_SET_LABELS_FILE.exists());
         Downloader.clearDownloadedFiles();
-        assertFalse(Downloader.MNIST_TRAIN_SET_IMAGES_FILE_PATH.exists());
-        assertFalse(Downloader.MNIST_TRAIN_SET_LABELS_FILE_PATH.exists());
-        assertFalse(Downloader.MNIST_TEST_SET_IMAGES_FILE_PATH.exists());
-        assertFalse(Downloader.MNIST_TEST_SET_LABELS_FILE_PATH.exists());
+        assertFalse(Downloader.MNIST_TRAIN_SET_IMAGES_FILE.exists());
+        assertFalse(Downloader.MNIST_TRAIN_SET_LABELS_FILE.exists());
+        assertFalse(Downloader.MNIST_TEST_SET_IMAGES_FILE.exists());
+        assertFalse(Downloader.MNIST_TEST_SET_LABELS_FILE.exists());
     }
 }
